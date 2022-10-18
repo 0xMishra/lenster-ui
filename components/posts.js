f11a68c5c0300604d484f55a4fb9ad1dc499c0c7
mishra811 <mishrakundan073@gmail.com>
2022-10-18 22:45:32 +0530

    added posts

--- components/posts.js
+++ components/posts.js
@@ -2,8 +2,14 @@ import React from "react";
 import Post from "./post";
 const Posts = () => {
   return (
-    <section className="mt-[220px] relative -top-64 z-30 flex-col justify-center items-center">
-      <Post />
+    <section className="flex justify-center items-center">
+      <div className="mt-[220px] relative -top-64 z-30 flex-col justify-center items-center">
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+      <div className="hidden lg:flex w-[50vw] max-w-[300px] bg-white relative z-30 "></div>
     </section>
   );
 };
